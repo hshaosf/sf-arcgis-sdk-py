e6bd446f9d1c6d5384e056c35eb63d4a48cbd290
Hui Shao <huiran.shao@sfgov.org>
2019-10-02 16:09:05 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,18 @@
-"""setup.py for boilerplate-sdk"""
+"""setup.py for sf-arcgis-sdk"""
 # pylint: disable=E0611,F0401
 #         No name 'core' in module 'distutils'
 #         Unable to import 'distutils.core'
 from distutils.core import setup
 import setuptools # pylint: disable=unused-import
-setup(name='boilerplate-sdk',
+setup(name='sf-arcgis-sdk',
       version='0.1.0',
       license='MIT',
       author='hshaosf',
       author_email='hshaosf@SFDigitalServices',
       url='https://github.com/SFDigitalServices',
-      packages=["boilerplate_sdk"],
+      packages=["sf_arcgis_sdk"],
       install_requires=[
-          'requests'
+          'requests',
+          'usaddress'
       ]
       )
